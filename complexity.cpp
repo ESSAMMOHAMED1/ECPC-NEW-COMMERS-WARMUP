7670efedf1778cf476720b2f91c752744c9dec76
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-11-07 20:48:02 -0800

    new update

---
+++ complexity.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+	int i, j, min_idx;
+	for (i = 0; i < n - 1; i++) {
+
+		min_idx = i;
+
+		for (j = i + 1; j < n; j++) {
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+			swap(arr[min_idx], arr[i]);
+	}
+}
+
+///================================================================================
+
+
+int main()
+{
+	int arr[] = { 64, 25, 12, 22, 11 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+}
+
--- dataTypes.cpp
+++ dataTypes.cpp
@@ -1,10 +1,5 @@
 void DataTypes(){
-   // int = 1e9
-   // long long = 1e18
-   // char
-   // string 
-   //double
-   //float
+   
    
 
 }
