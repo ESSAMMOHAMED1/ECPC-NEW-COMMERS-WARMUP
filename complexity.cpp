3c6a6ba0013659344f285389dcf1a8250e8d88aa
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-11-07 21:01:30 -0800

    new update

--- complexity.cpp
+++ complexity.cpp
@@ -3,21 +3,23 @@ using namespace std;
 
 void selectionSort(int arr[], int n)
 {
-	int i, j, min_idx;
-	for (i = 0; i < n - 1; i++) {
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++)
+    {
 
-		min_idx = i;
+        min_idx = i;
 
-		for (j = i + 1; j < n; j++) {
-			if (arr[j] < arr[min_idx])
-				min_idx = j;
-		}
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
 
-		if (min_idx != i)
-			swap(arr[min_idx], arr[i]);
-	}
+        if (min_idx != i)
+            swap(arr[min_idx], arr[i]);
+    }
 }
-
+/// O(n^2)
 ///================================================================================
 
 void bubbleSort(int arr[], int n)
@@ -25,12 +27,15 @@ void bubbleSort(int arr[], int n)
     int i, j;
     bool swapped;
 
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < n - 1; i++)
+    {
 
         swapped = false;
 
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
@@ -39,10 +44,28 @@ void bubbleSort(int arr[], int n)
             break;
     }
 }
+/// O(n^2)
+///====================================================================================
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
 
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+/// O(n^2)
+///=========================================================================================
 
 int main()
 {
-
 }
-
