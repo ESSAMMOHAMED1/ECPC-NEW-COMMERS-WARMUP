f8b9265931d83c0f0b2927026a3446741f8868a7
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-11-07 22:19:01 -0800

    new update

--- 2Darray.cpp
+++ 2Darray.cpp
@@ -1,23 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Access the Elements of a 2D array
+void TowDarrayAccessElement()
+{
+    string Names[2][4] = {
+        {"essam", "mohamed", "ali", "hossam"},
+        {"omnia", "ahmed", "omar", "sara"}};
 
+    Names[0][0] = "donia";
 
-
-///Loop Through a 2D array
-
-
-
-void TowDarray(){
-int players[20][20];
-for(int i = 0; i < 2; i++) {
-  for (int j = 0; j < 4; j++) {
-    cin>>players[i][j];
+    cout << Names[0][0];
 }
 
-for(int i = 0; i < 2; i++) {
-  for (int j = 0; j < 4; j++) {
-    cout << players[i][j] << "\n";
-  }
-}
+/// Loop Through a 2D array
+
+void TowDarrayLoopIn()
+{
+    int players[20][20];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> players[i][j];
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                cout << players[i][j] << "\n";
+            }
+        }
+    }
 }
